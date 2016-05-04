0317577c779d570a9c461e83a41cebc16b6ed589
Haukur Mar Bodvarsson <haukur@jivaro.com>
2016-05-04 11:30:36 +0000

    Adding Access-Control-Allow-Methods

--- src/server/index.js
+++ src/server/index.js
@@ -28,6 +28,7 @@ var gateway = braintree.connect({
 app.use(function (req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
 	next();
 });
 
