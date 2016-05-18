6d6db011ef4aab4f0f21150396987825f59e8485
Haukur Mar Bodvarsson <haukur@jivaro.com>
2016-05-18 16:21:54 +0000

    Moving component controllers into component file.

--- src/app/modules/braintree/components/presentation/braintree-customer/customer.component.js
+++ src/app/modules/braintree/components/presentation/braintree-customer/customer.component.js
@@ -1,12 +1,48 @@
 import template from './customer.html';
-import controller from './customer.controller';
+
+// Inject dependencies
+@Inject('$http', 'braintreeService')
+class CustomerComponent {
+	constructor() {
+		this.message = '';
+		this.isError = false;
+		this.isCreated = false;
+		this.showForm = true;
+
+		this.customerModel = {};
+	}
+
+	// Private methods
+	// --------------------------------------------------
+	$onInit() {
+		console.log('Braintree Customer Component $onInit...');
+	}
+
+	// Public viewModel methods
+	// --------------------------------------------------
+	createCustomer() {
+		this.message = 'Creating customer...';
+		this.showForm = false;
+
+		this.braintreeService.createCustomer(this.customerModel).then(
+			(success) => {
+				this.message = 'Customer created: ' + JSON.stringify(success.data);
+			},
+			(error) => {
+				// TODO: Handle errors better (use error.data.errors collection)
+				this.message = error.data.message;
+				console.log('Error message', error.data.message);
+				console.log('Errors:', error.data.errors);
+			}
+		);
+	}
+}
 
 // Component decorations
 let component = {
 	bindings: {},
-	template,
-	controller,
-	controllerAs: 'vm'
+	template: template,
+	controller: CustomerComponent
 };
 
 export default component;
--- src/app/modules/braintree/components/presentation/braintree-dropin/dropin.component.js
+++ src/app/modules/braintree/components/presentation/braintree-dropin/dropin.component.js
@@ -1,12 +1,85 @@
 import template from './dropin.html';
-import controller from './dropin.controller';
+
+// Inject dependencies
+@Inject('$http', 'braintreeService')
+class DropinComponent {
+	constructor() {
+		this.message = 'Please use the form below to pay:';
+		this.showDropinContainer = true;
+		this.isError = false;
+		this.isPaid = false;
+	}
+
+	// Private methods
+	// --------------------------------------------------
+	$onInit() {
+		console.log('Braintree Dropin Component...');
+		this._getToken();
+	}
+
+	_getToken() {
+		this.braintreeService.getClientToken().then(
+			(response) => {
+				console.log('res', response.data);
+
+				this.braintreeService.$braintree.setup(response.data.client_token, 'dropin', {
+					// id of html tag for braintree dropin container
+					container: 'js-braintree-checkout-container',
+					// Form is not submitted by default when paymentMethodNonceReceived is implemented
+					paymentMethodNonceReceived: (event, nonce) => {
+						this.message = 'Processing your payment...';
+						this.showDropinContainer = false;
+
+						let paymentData = {
+							amount: this.amount,
+							payment_method_nonce: nonce
+						};
+
+						// Process payment
+						this.braintreeService.processPayment(paymentData).then(
+							(response) => {
+								console.log('Success:', response.data);
+
+								if (response.data.success) {
+									this.message = 'Payment was authorized!';
+									this.showDropinContainer = false;
+									this.isError = false;
+									this.isPaid = true;
+
+								} else {
+									// TODO: Handle different payment failures
+									this.message = 'Payment failed: ' + response.data.message + ' Please refresh the page and try again.';
+									this.isError = true;
+								}
+
+							},
+							(error) => {
+								this.message = 'Error: cannot connect to server. Please make sure your server is running. Erromessage: ' + error.data;
+								this.showDropinContainer = false;
+								this.isError = true;
+
+							});
+
+					}
+				});
+			},
+			(error) => {
+				this.message = 'Error: cannot connect to server. Please make sure your server is running. Erromessage: ' + error.data;
+				this.showDropinContainer = false;
+				this.isError = true;
+			});
+
+	}
+
+	// Public viewModel methods
+	// --------------------------------------------------
+}
 
 // Component decorations
 let component = {
 	bindings: {},
-	template,
-	controller,
-	controllerAs: 'vm'
+	template: template,
+	controller: DropinComponent
 };
 
 export default component;
