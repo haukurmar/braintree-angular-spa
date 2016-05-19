b1c01bc21a41e25da3db7616efd07feef6d17ef3
Haukur Mar Bodvarsson <haukur@jivaro.com>
2016-05-19 13:54:06 +0000

    Moving controller into component file

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -1,15 +1,27 @@
 import template from './app.html';
-import controller from './app.controller';
+
+// Inject dependencies
+@Inject()
+export default class AppComponent {
+	constructor() {
+		this._activate();
+	}
+
+	// Class methods
+	// --------------------------------------------------
+	_activate() {
+		console.log('App Component...');
+	}
+}
 
 // Component decorations
 let component = {
 	bindings: {},
-	template,
-	controller,
-	controllerAs: 'vm',
+	template: template,
+	controller: AppComponent,
 	$routeConfig: [
-		{ path: '/', name: 'Home', component: 'appHome', useAsDefault: true },
-		{ path: '/braintree/...', name: 'BraintreeHome', component: 'braintreeHome' }
+		{path: '/', name: 'Home', component: 'appHome', useAsDefault: true},
+		{path: '/braintree/...', name: 'BraintreeHome', component: 'braintreeHome'}
 	]
 };
 
--- src/app/app.controller.js
+++ src/app/app.controller.js
@@ -1,13 +1,1 @@
-// Inject dependencies
-@Inject()
-export default class AppComponent {
-	constructor() {
-		this._activate();
-	}
 
-	// Class methods
-	// --------------------------------------------------
-	_activate() {
-		console.log('App Component...');
-	}
-}
