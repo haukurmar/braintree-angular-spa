e1e16962d45d35e0589c29708ce8c7510efd63bb
Haukur Mar Bodvarsson <haukur@jivaro.com>
2016-05-20 23:47:00 +0000

    Working version of subscription

--- src/app/modules/braintree/braintree.service.js
+++ src/app/modules/braintree/braintree.service.js
@@ -6,11 +6,131 @@ export default class BraintreeService {
 		this._tokenPath = '/v1/token';
 		this._processPath = '/v1/process';
 		this._customerPath = '/v1/customers';
+		this._subscriptionsPath = '/v1/subscriptions';
 		this._subscriptionPlansPath = '/v1/plans';
+		this._paymentMethodsPath = '/v1/paymentmethods';
+		this._customerData = {
+			customer: {}
+		};
+
+		window.customer = this._customerData;
 	}
 
+	// Private methods
+	// --------------------------------------------------
+
+
+	// Public methods
+	// --------------------------------------------------
 	get apiUrl() {
 		return this._apiUrl
+	}
+
+	get customer() {
+		return this._customerData.customer;
+	}
+
+	setup() {
+		this.getClientToken().then(
+			(response) => {
+				this.$braintree.setup(response.data.client_token, "custom");
+				console.log('Braintree setup with the clientToken', response.data.client_token);
+				let customer = {
+					clientToken: response.data.client_token
+				};
+
+				this.updateCustomer(customer);
+			});
+
+
+	}
+
+	/**
+	 * Tokenize creditcard and get paymentMethodNonce
+	 * @param paymentModel
+	 */
+	getPaymentMethodNonce(paymentModel) {
+		return new Promise((resolve, reject) => {
+			this.getClientToken().then(
+				(response) => {
+					// Create new client and tokenize card
+					let client = new this.$braintree.api.Client({clientToken: response.data.client_token});
+
+					client.tokenizeCard({
+						number: paymentModel.creditCardNumber,
+						expirationDate: paymentModel.expirationDate
+					}, (err, nonce) => {
+						// TODO: Handle errors better
+						if (err) {
+							reject(err);
+						}
+
+						resolve(nonce);
+					});
+				},
+				(error) => {
+					console.log('Error: cannot connect to server. Please try again. Erromessage: ' + error.data);
+					reject(error);
+				}
+			);
+		})
+	}
+
+	/**
+	 * Creating a new customer
+	 * @param customerData
+	 * @returns {*}
+	 */
+	createCustomer(customerData) {
+		return this.$http.post(this._apiUrl + this._customerPath, customerData);
+	}
+
+	// TODO: Probably change to take in the model
+	createSubscription() {
+		let subscriptionData = {
+			paymentMethodToken: this.customer.paymentMethod.token,
+			planId: this.customer.subscriptionPlan.id
+		};
+
+		console.log('this.customer.subscriptionPlan', this.customer.subscriptionPlan, subscriptionData);
+
+		// TODO: I need the paymentMethodToken here and not the nonce
+
+		console.log('NONCE', this.customer.paymentMethod.token, subscriptionData);
+
+		return this.$http.post(this._apiUrl + this._subscriptionsPath, subscriptionData);
+	}
+
+	createVaultedPayment(customerId, paymentModel) {
+		// Tokenize creditcard and get paymentMethodNonce
+		return new Promise((resolve, reject) => {
+			this.getPaymentMethodNonce(paymentModel).then(
+				(nonce) => {
+					let data = {
+						customerId: customerId,
+						paymentMethodNonce: nonce,
+						token: this.customer.paymentMethodToken
+					};
+
+					console.log('Vault payment data', data);
+
+					let customer = {
+						paymentMethodNonce: nonce
+					};
+
+					// Save the paymentMethodNonce to customer
+					this.updateCustomer(customer);
+
+					// Save to Vault
+					console.log('paymentMethodNonce and customer: ', data);
+					resolve(this.$http.post(this._apiUrl + this._paymentMethodsPath, data));
+				},
+				(error) => {
+					console.log('Failed to get PaymentMethodNonce', error);
+					reject('Failed to get PaymentMethodNonce', error);
+				}
+			);
+		});
 	}
 
 	getAllSubscriptionPlans() {
@@ -34,17 +154,34 @@ export default class BraintreeService {
 		return this.$http.post(this._apiUrl + this._processPath, paymentData);
 	}
 
-	/**
-	 * Creating a new customer
-	 * @param customerData
-	 * @returns {*}
-	 */
-	createCustomer(customerData) {
-		return this.$http.post(this._apiUrl + this._customerPath, customerData);
+	updateCustomer(customerModel) {
+		this.setObjectValues(this._customerData.customer, customerModel);
+		console.log('customer model updated in service', customerModel);
 	}
-
 
 	get $braintree() {
 		return braintree;
+	}
+
+
+	// TODO: Move somewhere else
+	// Walk throught the object tree and set values
+	setObjectValues(src, dest) {
+		for (var key in dest) {
+			if (!src.hasOwnProperty(key)) {
+				if (typeof dest[key] === 'object') {
+					src[key] = {};
+					this.setObjectValues(src[key], dest[key]);
+				} else {
+					src[key] = dest[key];
+				}
+			} else {
+				if (typeof dest[key] === 'object') {
+					this.setObjectValues(src[key], dest[key]);
+				} else {
+					src[key] = dest[key];
+				}
+			}
+		}
 	}
 }
--- src/app/modules/braintree/components/business/braintree-subscribe/subscribe.component.js
+++ src/app/modules/braintree/components/business/braintree-subscribe/subscribe.component.js
@@ -11,7 +11,8 @@ class SubscribeComponent {
 			showCreditCardForm: false,
 			showPlans: true,
 			showPaypalButton: false,
-			showMessage: false
+			showMessage: false,
+			showConfirmPanel: true
 		};
 
 		// TODO: Move to service
@@ -25,6 +26,8 @@ class SubscribeComponent {
 	// Private methods
 	// --------------------------------------------------
 	$onInit() {
+		this.braintreeService.setup();
+
 		this._getAllSubscriptionPlans();
 	}
 
@@ -41,27 +44,52 @@ class SubscribeComponent {
 		);
 	}
 
+	confirmSubscription() {
+		let customer = this.braintreeService.customer;
+		this.message = JSON.stringify(customer);
+
+		this.braintreeService.createSubscription().then(
+			(response) => {
+				if (response.data.success) {
+					this.message = 'Subscription was created!';
+				} else {
+					// TODO: Handle different failures maybe?
+					this.message = 'An error occurred creating a subscription: ' + response.data.message;
+				}
+			},
+			(error) => {
+				this.message = error.data.message;
+			}
+		);
+	}
+
 	// Public viewModel methods
 	// --------------------------------------------------
 	chooseSubscriptionPlan(subscriptionPlanModel) {
-		this.subscription.plan = subscriptionPlanModel;
+		console.log('plan chosen', subscriptionPlanModel);
+
 		this.state.showPlans = false;
 		this.state.showCustomerForm = true;
-		console.log('plan chosen', subscriptionPlanModel);
+
+		let customer = {
+			subscriptionPlan: subscriptionPlanModel
+		};
+
+		this.braintreeService.updateCustomer(customer);
 	}
 
-
 	createCustomer(customerModel) {
-		console.log('Creating customer from subscribe', customerModel);
 		this.message = 'Creating customer...';
 		this.state.showMessage = true;
 		this.state.showCustomerForm = false;
 
 		this.braintreeService.createCustomer(customerModel).then(
 			(response) => {
-				console.log('Customer created: ' + JSON.stringify(response.data));
+				console.log('Customer created: ', response.data);
 				this.message = '';
 				this.state.showCreditCardForm = true;
+
+				this.braintreeService.updateCustomer(response.data.customer);
 			},
 			(error) => {
 				this.message = error.data.message;
@@ -74,57 +102,37 @@ class SubscribeComponent {
 		);
 	}
 
-	processPayment(cardModel) {
-		this.message = 'Processing payment...';
+	tokenizeCreditcard(paymentModel) {
+		this.message = 'Validating creditcard...';
 		this.state.showCreditCardForm = false;
 
 		// Send request to get token, then use the token to tokenize credit card info and process a transaction
 		this.braintreeService.getClientToken().then(
 			(response) => {
-				// TODO: Use Existing customer that was created in last step
-
 				// Create new client and tokenize card
 				let client = new this.braintreeService.$braintree.api.Client({clientToken: response.data.client_token});
 
 				client.tokenizeCard({
-					number: cardModel.creditCardNumber,
-					expirationDate: cardModel.expirationDate
+					number: paymentModel.creditCardNumber,
+					expirationDate: paymentModel.expirationDate
 				}, (err, nonce) => {
 					// TODO: Handle errors better
 					if (err) {
-						this.message = 'An error occurred tokenizing card: ' + JSON.stringify(err);
-						this.state.showCreditCardForm = true;
 						return;
 					}
 
-					let paymentData = {
-						amount: cardModel.amount,
-						payment_method_nonce: nonce
+					let customer = {
+						paymentMethodNonce: nonce
 					};
 
-					// Process payment
-					this.braintreeService.processPayment(paymentData).then(
-						(response) => {
-							if (response.data.success) {
-								this.message = 'Payment was authorized!';
-								this.isError = false;
-								this.isPaid = true;
+					// Save paymentMethodNonce to customerModel in service
+					this.braintreeService.updateCustomer(customer);
+				});
 
-							} else {
-								// TODO: Handle different payment failures
-								this.message = 'Payment failed: ' + response.data.message + ' Please refresh the page and try again.';
-								this.isError = true;
-								this.state.showCreditCardForm = true;
-							}
+				// TODO: I want to be sure that tokenizeCard worked
+				this.message = '';
+				this.state.showConfirmPanel = true;
 
-						},
-						(error) => {
-							this.message = 'Error: cannot connect to server. Please try again. Erromessage: ' + error.data;
-							this.isError = true;
-							this.state.showCreditCardForm = true;
-						}
-					);
-				})
 			},
 			(error) => {
 				this.message = 'Error: cannot connect to server. Please try again. Erromessage: ' + error.data;
@@ -133,6 +141,94 @@ class SubscribeComponent {
 			}
 		);
 	}
+
+	createVaultedPayment(paymentModel) {
+		let customerId = this.braintreeService.customer.id;
+
+		this.message = 'Validating creditcard...';
+		this.state.showCreditCardForm = false;
+
+		this.braintreeService.createVaultedPayment(customerId, paymentModel).then(
+			(success) => {
+				console.log('VAULT success!', success);
+
+				// Save the paymentMethodNonce to customer
+				this.braintreeService.updateCustomer(success.data.customer);
+
+				// TODO: We cannot update viewmodel here
+				this.message = '';
+				this.state.showConfirmPanel = true;
+			},
+			(error) => {
+				console.log('ERRROR', error);
+				// TODO: We cannot update viewmodel here
+				this.message = JSON.stringify(error);
+			}
+		);
+
+		// Send request to get token, then use the token to tokenize credit card info and process a transaction
+
+	}
+
+	// processPayment(cardModel) {
+	// 	this.message = 'Processing payment...';
+	// 	this.state.showCreditCardForm = false;
+	//
+	// 	// Send request to get token, then use the token to tokenize credit card info and process a transaction
+	// 	this.braintreeService.getClientToken().then(
+	// 		(response) => {
+	// 			// TODO: Use Existing customer that was created in last step
+	//
+	// 			// Create new client and tokenize card
+	// 			let client = new this.braintreeService.$braintree.api.Client({clientToken: response.data.client_token});
+	//
+	// 			client.tokenizeCard({
+	// 				number: cardModel.creditCardNumber,
+	// 				expirationDate: cardModel.expirationDate
+	// 			}, (err, nonce) => {
+	// 				// TODO: Handle errors better
+	// 				if (err) {
+	// 					this.message = 'An error occurred tokenizing card: ' + JSON.stringify(err);
+	// 					this.state.showCreditCardForm = true;
+	// 					return;
+	// 				}
+	//
+	// 				let paymentData = {
+	// 					amount: cardModel.amount,
+	// 					payment_method_nonce: nonce
+	// 				};
+	//
+	// 				// Process payment
+	// 				this.braintreeService.processPayment(paymentData).then(
+	// 					(response) => {
+	// 						if (response.data.success) {
+	// 							this.message = 'Payment was authorized!';
+	// 							this.isError = false;
+	// 							this.isPaid = true;
+	//
+	// 						} else {
+	// 							// TODO: Handle different payment failures
+	// 							this.message = 'Payment failed: ' + response.data.message + ' Please refresh the page and try again.';
+	// 							this.isError = true;
+	// 							this.state.showCreditCardForm = true;
+	// 						}
+	//
+	// 					},
+	// 					(error) => {
+	// 						this.message = 'Error: cannot connect to server. Please try again. Erromessage: ' + error.data;
+	// 						this.isError = true;
+	// 						this.state.showCreditCardForm = true;
+	// 					}
+	// 				);
+	// 			})
+	// 		},
+	// 		(error) => {
+	// 			this.message = 'Error: cannot connect to server. Please try again. Erromessage: ' + error.data;
+	// 			this.isError = true;
+	// 			this.state.showCreditCardForm = true;
+	// 		}
+	// 	);
+	// }
 }
 
 // Component decorations
