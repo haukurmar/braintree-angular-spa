e85c705bbc3074c335215e484a5fa3e1a9f25d3d
Haukur Mar Bodvarsson <haukur@jivaro.com>
2016-06-02 15:05:27 +0000

    refactor naming of components in imports

--- src/app/modules/braintree/braintree.module.js
+++ src/app/modules/braintree/braintree.module.js
@@ -9,16 +9,16 @@ import UICreditCardFormComponent from './components/presentation/creditcard-form
 import UICustomerFormComponent from './components/presentation/customer-form/customer-form.component';
 import UISubscriptionPlanComponent from './components/presentation/subscription-plan/subscription-plan.component';
 import UISubscriptionOverviewComponent from './components/presentation/subscription-overview/subscription-overview.component';
-import SubscriptionProgressPresentationComponent from './components/presentation/subscription-progress/subscription-progress.component';
+import UISubscriptionProgressComponent from './components/presentation/subscription-progress/subscription-progress.component';
 
 // Container Components
-import CustomerContainerComponent from './components/container/customer/customer.component';
-import CreditCardContainerComponent from './components/container/creditcard/creditcard.component';
-import DropinContainerComponent from './components/container/dropin/dropin.component';
-import PaypalContainerComponent from './components/container/paypal/paypal.component';
-import SubscriptionContainerComponent from './components/container/subscription/subscription.component';
-import SubscriptionOverviewContainerComponent from './components/container/subscription-overview/subscription-overview.component';
-import PaymentMethodsContainerComponent from './components/container/payment-methods/payment-methods.component';
+import CustomerComponent from './components/container/customer/customer.component';
+import CreditCardComponent from './components/container/creditcard/creditcard.component';
+import DropinComponent from './components/container/dropin/dropin.component';
+import PaypalComponent from './components/container/paypal/paypal.component';
+import SubscriptionComponent from './components/container/subscription/subscription.component';
+import SubscriptionOverviewComponent from './components/container/subscription-overview/subscription-overview.component';
+import PaymentMethodsComponent from './components/container/payment-methods/payment-methods.component';
 
 // View Components (Route Components)
 import HomeViewComponent from './components/view/braintree-home/braintree-home.component';
@@ -32,18 +32,18 @@ let ngModule = angular.module('braintree', [
  */
 ngModule
 	.service('braintreeService', BraintreeService)
-	.component('braintreeCustomer', CustomerContainerComponent)
-	.component('braintreeCreditCard', CreditCardContainerComponent)
-	.component('braintreeDropin', DropinContainerComponent)
-	.component('braintreeSubscription', SubscriptionContainerComponent)
-	.component('braintreeSubscriptionOverview', SubscriptionOverviewContainerComponent)
-	.component('braintreePaymentMethods', PaymentMethodsContainerComponent)
-	.component('braintreePaypal', PaypalContainerComponent)
+	.component('braintreeCustomer', CustomerComponent)
+	.component('braintreeCreditCard', CreditCardComponent)
+	.component('braintreeDropin', DropinComponent)
+	.component('braintreeSubscription', SubscriptionComponent)
+	.component('braintreeSubscriptionOverview', SubscriptionOverviewComponent)
+	.component('braintreePaymentMethods', PaymentMethodsComponent)
+	.component('braintreePaypal', PaypalComponent)
 	.component('braintreeHome', HomeViewComponent)
 	.component('uiBraintreeCustomerForm', UICustomerFormComponent)
 	.component('uiBraintreeCreditcardForm', UICreditCardFormComponent)
 	.component('uiBraintreeSubscriptionPlan', UISubscriptionPlanComponent)
-	.component('uiBraintreeSubscriptionProgress', SubscriptionProgressPresentationComponent)
+	.component('uiBraintreeSubscriptionProgress', UISubscriptionProgressComponent)
 	.component('uiBraintreeSubscriptionOverview', UISubscriptionOverviewComponent);
 
 export default ngModule;
