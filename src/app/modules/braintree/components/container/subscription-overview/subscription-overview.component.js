a77246e58c06cba3022afe010086e4094002f7a4
Haukur Mar Bodvarsson <haukur@jivaro.com>
2016-08-29 17:51:54 +0000

    Adding discount from selectedSubscription when creating a subscription.

--- src/app/modules/braintree/components/container/subscription-overview/subscription-overview.component.js
+++ src/app/modules/braintree/components/container/subscription-overview/subscription-overview.component.js
@@ -1,4 +1,5 @@
 import template from './subscription-overview.html';
+import _ from 'underscore';
 
 // Inject dependencies
 @Inject('braintreeDataService', 'braintreeAppService')
@@ -21,7 +22,7 @@ class SubscriptionOverviewComponent {
 	// --------------------------------------------------
 	$onInit() {
 		let customer = this.braintreeDataService.customer;
-		if(customer.clientToken) {
+		if (customer.clientToken) {
 			this._confirmSubscription();
 		}
 	}
@@ -41,8 +42,32 @@ class SubscriptionOverviewComponent {
 		};
 
 		// If billing date is set in the future (for promotions and such)
-		if(this.selectedSubscription.firstBillingDate) {
+		if (this.selectedSubscription.firstBillingDate) {
 			subscriptionData.subscription.firstBillingDate = this.selectedSubscription.firstBillingDate;
+		}
+
+		// Find if there are any currency discounts
+		if (this.selectedSubscription.currencyDiscounts && this.selectedSubscription.currencyDiscounts.length) {
+			// Get the currencyIsoCode for the selected merchant account
+			let selectedCurrency = this.braintreeDataService.getCurrencyForMerchantAccountId(this.selectedMerchantAccount.id);
+
+			// Find the discount for this currency
+			let currencyDiscount = _.find(this.selectedSubscription.currencyDiscounts, {currencyIsoCode: selectedCurrency});
+
+			if (currencyDiscount) {
+				let discount = {
+					inheritedFromId: currencyDiscount.discountCodePrefix + selectedCurrency
+				};
+
+				// Add Discount to subscriptionData
+				this.braintreeDataService.addDiscountToSelectedSubscription(discount);
+			}
+
+		}
+
+		// Adding discount to subscription data
+		if (this.selectedSubscription.discounts && this.selectedSubscription.discounts.add && this.selectedSubscription.discounts.add.length) {
+			subscriptionData.subscription.discounts = this.selectedSubscription.discounts
 		}
 
 		this.braintreeDataService.createSubscription(subscriptionData).then(
@@ -73,7 +98,7 @@ class SubscriptionOverviewComponent {
 		);
 	}
 
-	routeTo(path){
+	routeTo(path) {
 		return this.braintreeAppService.routeTo(path);
 	}
 }
@@ -81,7 +106,7 @@ class SubscriptionOverviewComponent {
 // Component decorations
 let component = {
 	bindings: {},
-	template : template,
+	template: template,
 	controller: SubscriptionOverviewComponent
 };
 
