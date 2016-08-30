fec60a5ce59b33835a87466de9c685c35929a92b
Haukur Mar Bodvarsson <haukur@jivaro.com>
2016-08-30 18:33:40 +0000

    Use local api

--- src/app/modules/braintree/services/braintree-config.service.js
+++ src/app/modules/braintree/services/braintree-config.service.js
@@ -2,8 +2,8 @@
 @Inject()
 export default class ConfigService {
 	constructor() {
-		this._apiUrl = 'https://haukurmar-braintree-node-api.herokuapp.com/api';
-		//this._apiUrl = 'http://127.0.0.1:5000/api';
+		//this._apiUrl = 'https://haukurmar-braintree-node-api.herokuapp.com/api';
+		this._apiUrl = 'http://127.0.0.1:5000/api';
 	}
 
 	get apiUrl() {
