d50bdcf07fa4c85a19599d8a78696cb75b90713e
Haukur Mar Bodvarsson <haukur@jivaro.com>
2016-09-09 13:48:27 +0000

    Better message for Do Not Honor error.

--- src/app/modules/braintree/braintree.constants.js
+++ src/app/modules/braintree/braintree.constants.js
@@ -14,7 +14,7 @@ const GATEWAY_ERRORS = [
 	{
 		code: '2000',
 		text: 'Do Not Honor',
-		description: 'Your bank is unwilling to accept the transaction. Contact your bank for more details regarding this generic decline.'
+		description: 'Your bank is unwilling to accept the transaction. Try choosing a different currency - if the message persists you will need to contact your bank for more details regarding this generic decline.'
 	},
 	{
 		code: '2001',
